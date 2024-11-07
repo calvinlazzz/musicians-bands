ce90ae66857d188d586a2b905d4def240c084519
Calvin La <calvin.la@clover.com>
2024-11-07 13:40:05 -0800

    Added associations and relevant tests for the associations

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
-
+Band.hasMany(Musician)
+Musician.belongsTo(Band)
+Band.belongsToMany(Song, { through: 'BandSongs' })
+Song.belongsToMany(Band, { through: 'BandSongs' })
 
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -15,6 +15,8 @@ describe('Band, Musician, and Song Models', () => {
     
 
     test('can create a Band', async () => {
+        await sequelize.sync({ force: true });
+
         const band = await Band.create({ name: 'Jonas Brothers', genre: 'Rock' });
 
         expect(band.name).toBe('Jonas Brothers');
@@ -23,12 +25,15 @@ describe('Band, Musician, and Song Models', () => {
     })
 
     test('can create a Musician', async () => {
+        await sequelize.sync({ force: true });
+
         // TODO - test creating a musician
         const musician = await Musician.create({ name: 'Polo G', instrument: 'Piano' });
         expect(musician.name).toBe('Polo G');
         expect(musician.instrument).toBe('Piano');  
     })
     test('can create a Song', async () => {
+        await sequelize.sync({ force: true });
         // TODO - test creating a musician
         const song = await Song.create({ title: 'Rapstar', length: 3, year: 2021 });
         expect(song.title).toBe('Rapstar');
@@ -37,24 +42,30 @@ describe('Band, Musician, and Song Models', () => {
     })
 
     test('can update a Band', async () => {
-    
+        await sequelize.sync({ force: true });
         const band = await Band.create({ name: 'Jonas Brothers', genre: 'Rock' });
         const updatedBand = await band.update({ genre: 'Pop' });
         expect(band.genre).toBe('Pop');
     })
 
     test('can update a Musician', async () => {
+        await sequelize.sync({ force: true });
+
         const musician = await Musician.create({ name: 'Polo G', instrument: 'Piano' });
         const updatedMusician = await musician.update({ instrument: 'Guitar' });
         expect(musician.instrument).toBe('Guitar');
     })
     test('can update a Song', async () => {
+        await sequelize.sync({ force: true });
+
         const song = await Song.create({ title: 'Rapstar', length: 3, year: 2021 });
         const updatedSong = await song.update({ length: 4 });
         expect(song.length).toBe(4);
     })
 
     test('can delete a Band', async () => {
+        await sequelize.sync({ force: true });
+
         const band = await Band.create({ name: 'Jonas Brothers', genre: 'Rock' });
         console.log(Band.length);
         await band.destroy();
@@ -63,16 +74,51 @@ describe('Band, Musician, and Song Models', () => {
     })
 
     test('can delete a Musician', async () => {
-        // TODO - test deleting a musician
+        await sequelize.sync({ force: true });
+
         const musician = await Musician.create({ name: 'Polo G', instrument: 'Piano' });
         await musician.destroy();
         const foundMusician = await Musician.findByPk(musician.id);
         expect(foundMusician).toBeNull();
     })
     test('can delete a Song', async () => {
+        await sequelize.sync({ force: true });
+
         const song = await Song.create({ title: 'Rapstar', length: 3, year: 2021 });
         await song.destroy();
         const foundSong = await Song.findByPk(song.id);
         expect(foundSong).toBeNull();
     })
+    test('Musicians many to one Band', async () => {
+        await sequelize.sync({ force: true });
+
+        // TODO - test the association between musicians and bands
+        const band = await Band.create({ name: 'Jonas Brothers', genre: 'Rock' });
+        const musician = await Musician.create({ name: 'Polo G', instrument: 'Piano'});
+        const musician2 = await Musician.create({ name: 'Lil Baby', instrument: 'Guitar'});
+        await band.addMusician(musician);
+        await band.addMusician(musician2);
+        const musicians = await band.getMusicians();
+        expect(musicians.length).toBe(2);
+    })
+    test('Songs many to many Bands', async () => {
+        await sequelize.sync({ force: true });
+
+        // TODO - test the association between songs and bands
+        const band = await Band.create({ name: 'Jonas Brothers', genre: 'Rock' });
+        const band2 = await Band.create({ name: 'Migos', genre: 'Rap' });
+        const song = await Song.create({ title: 'Rapstar', length: 3, year: 2021 });
+        const song2 = await Song.create({ title: 'Rapstar2', length: 4, year: 2022 });
+        await band.addSong(song);
+        await band.addSong(song2);
+        await band2.addSong(song);
+        await band2.addSong(song2);
+        
+        const songs = await band.getSongs();
+        const songs2 = await band2.getSongs();
+        expect(songs.length).toBe(2);
+        expect(songs2.length).toBe(2);
+    
+    })
+    
 })
